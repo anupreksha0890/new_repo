96c2290a56b85c47e1d1e7d7e17a0fbedec2bc8d
anupreksha0890 <anupreksha0890@gmail.com>
2025-12-10 13:35:01 +0530

    Update BusinessProjectApplication.java

--- src/main/java/com/business/BusinessProjectApplication.java
+++ src/main/java/com/business/BusinessProjectApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class BusinessProjectApplication {
+public class BusinessProjectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args)
 	{
